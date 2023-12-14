e89d9ac7a5118c49f0624cd4db6b870b8326dc66
sachin <sachinp@iitbhilai.ac.in>
2023-12-14 11:56:54 +0530

    update

--- index.js
+++ index.js
@@ -13,49 +13,99 @@ const requestLogger = (request, response, next) => {
   next()
 }
 app.use(requestLogger)
-const baseUrl = 'http://localhost:3001/api/notes'
+// const baseUrl = 'http://localhost:3001/api/persons'
 
-let notes = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 5,
-    "name": "Mary ",
-    "number": "39-12-6423122"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+  let persons =[
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "Sachin",
+      "number": "6205042648",
+      "id": 4
+    },
+    {
+      "name": "ranji",
+      "number": "65821",
+      "id": 5
+    },
+    {
+      "name": "sdfhyj",
+      "number": "65892",
+      "id": 6
+    },
+    {
+      "name": "sachin",
+      "number": "5644",
+      "id": 7
+    },
+    {
+      "name": "sjghhj",
+      "number": "658",
+      "id": 8
+    },
+    {
+      "name": "",
+      "number": "",
+      "id": 9
+    },
+    {
+      "name": "ramji ",
+      "number": "65848",
+      "id": 10
+    },
+    {
+      "name": "uykglkj",
+      "number": "03989",
+      "id": 11
+    },
+    {
+      "name": "fgxj",
+      "number": "56+",
+      "id": 12
+    },
+    {
+      "name": "sachin praksh ",
+      "number": "2698456265",
+      "id": 13
+    },
+    {
+      "name": "rani singh",
+      "number": "6524856525",
+      "id": 14
+    }
+  ]
 
-let p = notes.length
+let p = persons.length
 const dt = new Date()
 app.get('/info', (request, response) => {
   response.send(`<h1>Total Number of the element in this Phonebook is : ${p}</h1> <h2> ${dt}<h2> `)
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id == id)//in js === does not give true for 1 and '1' as one is int and other is string tht's y used ==
+  const note = persons.find(note => note.id == id)//in js === does not give true for 1 and '1' as one is int and other is string tht's y used ==
   // console.log(note)
   if (note) {
     response.json(note)
@@ -65,19 +115,19 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end()
   }
 })
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(note => note.id !== id)
   response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const note = request.body
   console.log(note)
   // console.log(note.name)
   const nm = note.name
   const id = note.id
-  const flag = notes.find(note => note.name == nm || note.id == id)
+  const flag = persons.find(note => note.name == nm || note.id == id)
   if (flag) {
     response.send(`The duplicate of this already exist in this phonebook :`)
     response.status(204).end()
